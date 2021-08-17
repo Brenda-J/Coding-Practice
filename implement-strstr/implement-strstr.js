27bca25e2c2cb0743554ba685458fa53a742dbab
Brenda <linjiangda@yahoo.com>
2021-08-16 21:16:48 -0400

    Time: 80 ms (57.73%), Space: 40.6 MB (36.31%) - LeetHub

--- implement-strstr/implement-strstr.js
+++ implement-strstr/implement-strstr.js
@@ -5,6 +5,7 @@
  */
 var strStr = function(haystack, needle) {
       if (needle.length === 0) return 0;
+    
     for (let i = 0; i < haystack.length; i++) {
         if (haystack.substr(i, needle.length) === needle) {  
             return i;  
