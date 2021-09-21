16f4a2590888676f34dddd8465f7b2912c59aa7c
Brenda <linjiangda@yahoo.com>
2021-09-20 20:44:50 -0400

    Time: 147 ms (28.75%), Space: 38.9 MB (96.07%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,11 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
+   let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j <nums.length; j++) {
             if (nums[i] + nums[j] === target) {
-                return [i, j]
+                result.push(i, j)
             }
         }
     }
+    return result;
 };
